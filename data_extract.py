f151f07b4297fd084770d7a6cdbbe0ade789fbe3
Saurav Saha <contact.srvsaha@gmail.com>
2016-09-16 17:31:51 +0530

    EVALITA 2016 QA-FAQ  EXTRACT DATA

---
+++ data_extract.py
@@ -0,0 +1,18 @@
+#
+#   @author      : SRvSaha
+#   Filename     : data_extract.py
+#   Timestamp    : 15:57 16-September-2016 (Friday)
+#   Description  : Data Extraction for EVALITA Question Answering FAQ
+#
+import re
+import sys
+
+filename = sys.argv[1]
+with open(filename) as f:
+    lines = f.readlines()
+for line in lines[1:]:
+    file = line[:3]
+    output = line[4:]
+    with open(file,'w') as f:
+        f.write(output)
+print("Operation Successful :)")
