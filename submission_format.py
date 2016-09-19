38327fd251d6946f44bcab3f7b3ee0c2f54f4111
Saurav Saha <contact.srvsaha@gmail.com>
2016-09-19 13:00:05 +0530

    Run 2 Submission Format || EVALITA 2016

--- submission_format.py
+++ submission_format.py
@@ -8,16 +8,18 @@ import sys
 
 file = sys.argv[1]
 
-f_out = open("final_submission_format.txt", 'w')
+f_out = open("RUN2-VERSION-3.txt", 'w')
 with open(file) as f:
     lines = f.readlines()
 output = []
 for line in lines:
     splitted = line.split("\t")
     count = 1
+    #print(splitted,len(splitted))
     if splitted[1] != "null\n":
-        for item in splitted[1:-1]:
+        if(len(splitted)==2):
             if count <= 25:
+                item = splitted[1]
                 if '\n' not in item:
                     answer = splitted[0]+"\t"+item[-7:-4]+"\t"+str(count)+"\n"
                 else:
@@ -26,5 +28,16 @@ for line in lines:
             else:
                 break
             count += 1
+        else:
+            for item in splitted[1:-1]:
+                if count <= 25:
+                    if '\n' not in item:
+                        answer = splitted[0]+"\t"+item[-7:-4]+"\t"+str(count)+"\n"
+                    else:
+                        answer = splitted[0]+"\t"+item[-8:-5]+"\t"+str(count)+"\n"
+                    output.append(answer)
+                else:
+                    break
+                count += 1
 f_out.writelines(output)
 print("Operation Successful :)")
